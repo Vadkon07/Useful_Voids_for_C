17e12e7bda236dd8586c9ee17de1fba34bab68db
Vadkon07 <vadkonbeats@gmail.com>
2024-09-13 09:11:12 +0000

    Update

---
+++ main.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include "templates.h"
+
+int main() {
+	generateRandomNumber(1, 5);
+	pauseProgram(5, 1); // If you don't want to see a print output, instead of (5, 1) set (5, 0)
+}
--- templates.h
+++ templates.h
@@ -1,25 +1,56 @@
-//USAGE:
-//
-//Open your project, and add in the beggining #include "templates", then just call functions from this file by entering something in a kind of 'clearScreen();'
-//
+#ifndef UTILS_H
+#define UTILS_H
 
-#ifndef GAME_H
-#define GAME_H
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <ctype.h>
+#include <unistd.h>
 
-
-
-void clearScreen(){
-#ifdef _WIN32
-	system("cls");
-#else
-	system("clear");
-#endif
+// Clears the screen based on the operating system
+void clearScreen() {
+    #ifdef _WIN32
+        system("cls");
+    #else
+        system("clear");
+    #endif
 }
 
-void waitForUser(){
-	printf("Press Enter to continue...\n");
-	getchar();
+// Waits for the user to press Enter
+void waitForUser() {
+    printf("Press Enter to continue...\n");
+    getchar();
 }
 
+// Generates a random number between min and max
+void generateRandomNumber(int min, int max) {
+    srand(time(0));
+    int num = (rand() % (max - min + 1)) + min;
+    printf("Random Number: %d\n", num);
+}
+
+// Pauses the program for a specified number of seconds
+void pauseProgram(int seconds, int show) {
+    if (show == 1) {
+	    printf("Pausing for %d seconds...\n", seconds);
+	    sleep(seconds);
+    } else {
+	    sleep(seconds);
+    }
+}
+
+// Prints a line of dashes
+void printLine() {
+    printf("------------------------------\n");
+}
+
+// Converts a string to uppercase
+void toUpperCase(char *str) {
+    while (*str) {
+        *str = toupper(*str);
+        str++;
+    }
+}
 
 #endif
+
